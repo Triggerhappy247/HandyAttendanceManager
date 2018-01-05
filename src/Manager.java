ef90caf901e4cd9c9945a606f26b4674e01f05f0
Qasim <qasim.bhatia@gmail.com>
2018-01-05 22:08:35 +0530

    Testing tesing

--- src/Manager.java
+++ src/Manager.java
@@ -1,6 +1,6 @@
 public class Manager {
     public static void main(String args[])
     {
-        System.out.println("Hello");
+        System.out.println("Hello Yashaswi, This is cool.");
     }
 }
