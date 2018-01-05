3f06d3039f359ced56f429589c60d4baabb14178
Yashaswi <16bce108@nirmauni.ac.in>
2018-01-05 22:30:06 +0530

    code updated

--- src/Manager.java
+++ src/Manager.java
@@ -1,6 +1,10 @@
 public class Manager {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         System.out.println("Hello Yashaswi, This is cool.");
+
+        int a;
+        a = 5;
+        System.out.println(a);
     }
+
 }
