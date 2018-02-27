7e96d323f124cabe552095d300feb5620149eb62
Qasim <qasim.bhatia@gmail.com>
2018-02-27 21:43:49 +0530

    Tested JavaFX Window.

--- src/HandyManager.java
+++ src/HandyManager.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class HandyManager extends Application {
@@ -9,6 +12,9 @@ public class HandyManager extends Application {
 
 
     public void start(Stage primaryStage) {
-
+        Parent root = new BorderPane();
+        primaryStage.setTitle("Handy Attendance Manager");
+        primaryStage.setScene(new Scene(root, 500, 300));
+        primaryStage.show();
     }
 }
