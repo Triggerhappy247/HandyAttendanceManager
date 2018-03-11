b8a44821ed0d2064525b7c7611453f3b19bee910
Qasim <qasim.bhatia@gmail.com>
2018-03-11 19:17:16 +0530

    Created TimeTable Window.

--- src/LoginController.java
+++ src/LoginController.java
@@ -8,8 +8,6 @@ import javafx.scene.control.TextField;
 public class LoginController {
 
     @FXML
-    private TextArea outputArea;
-    @FXML
     private TextField username;
     @FXML
     private PasswordField password;
@@ -28,19 +26,12 @@ public class LoginController {
         DatabaseConnection db = new DatabaseConnection();
         db.connect("jdbc:mysql://localhost:3310/attendance_manager?useSSL=false");
         Faculty faculty = new Faculty(username.getText(),password.getText(),db);
-        StringBuilder output =  new StringBuilder("Id = " + faculty.getIdFaculty());
-        output.append("\nPass = " + faculty.getPassword());
-        output.append("\nSubjects");
-        for (Subject subject : faculty.getSubjects()){
-            output.append(subject.getIdSubject() + " " + subject.getSubName() + "\n");
-        }
 
-        output.append("Time Slots\n");
-        for(TimeTableSlot slots : faculty.getTimeTable().getSlotIds()){
-            output.append(slots.getIdTimeTableSlot() + " " + slots.getSubject().getSubName()+ " " + slots.getDayOfWeek() + " " + slots.getTime().toString() + " " + slots.getRoom() + " " + slots.getStudentList() + " " + slots.getSlotType() + "\n");
+        if(faculty != null)
+        {
 
         }
-        outputArea.setText(output.toString());
+
         db.close();
     }
 
