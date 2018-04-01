e0d9f26ecec0d43c4f2aa8faa38f1760c8f621e1
Qasim <qasim.bhatia@gmail.com>
2018-04-01 16:03:18 +0530

    Optimized imports.

--- src/TimeTableViewController.java
+++ src/TimeTableViewController.java
@@ -7,7 +7,6 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.text.TextAlignment;
-import javafx.stage.Popup;
 
 import java.net.URL;
 import java.text.SimpleDateFormat;
